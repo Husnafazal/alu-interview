19826a70cf6a4105e8baf322df8f2f9c8b97a136
Husnafazal <101583586+Husnafazal@users.noreply.github.com>
2023-02-09 22:19:17 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,19 +4,25 @@ Calculates the minimum number of Copy and Paste operations needed to get n H cha
 """
 
 
-def minOperations(n)
+def minOperations(n):
+    """ If n is less than or equal to 1, then it is already achieved, so return 0 operations"""
     if n <= 1:
         return 0
     
     operations = 0
     i = 2
+    """Loop until i squared is greater than n"""
     while i * i <= n:
+        """If n is divisible by i, increase operations by 2 and divide n by i"""
         while n % i == 0:
             operations += 2
             n = n // i
         i += 1
     
+    """If n is still greater than 1, increment operations by 1"""
     if n > 1:
         operations += 1
     
-    return operations
+    """Return the total number of operations"""
+    return operations
+
