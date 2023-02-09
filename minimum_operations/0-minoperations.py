0491866c31bc23999631457ffb6b081b8bc1c0de
Husnafazal <101583586+Husnafazal@users.noreply.github.com>
2023-02-09 22:29:44 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 """
-Calculates the minimum number of Copy and Paste operations needed to get n H characters in a text file
+This is a function that calculates the minimum number
+of operations needed to result in exactly n 'H' characters in a text file.
 """
 
 
 def minOperations(n):
-    """If n is less than or equal to 1, return 0"""
-    if n <= 1:
+    """ return 0 if n is less than or equal to 0"""
+    if n <= 0:
         return 0
-    
-    """Initialize operations count"""
     operations = 0
-    
-    """Starting with 2, as 1 is not a factor"""
     i = 2
-    
-    """Loop until i*i is greater than n"""
-    while i * i <= n:
-        """Keep dividing n by i until n % i is not 0"""
+    """ iterate through all possible divisors of n"""
+    while i <= n:
+        """add the divisor to operations count and divide n by it
+         until n is no longer divisible by it"""
         while n % i == 0:
-            """Increase the operations count by 2 for each division by i"""
-            operations += 2
+            operations += i
             n = n // i
         i += 1
-    
-    if n > 1:
-        operations += 1
-    
+    """ return the total number of operations"""
     return operations
